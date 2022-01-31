12ca4938a192efec46df9d96a44bf88dd818a1c7
DongsooKim <hiswing91@gmail.com>
2022-02-01 03:45:37 +0900

    초기 세팅

--- src/main/java/com/danielsoo/blog_daniel/web/IndexController.java
+++ src/main/java/com/danielsoo/blog_daniel/web/IndexController.java
@@ -3,10 +3,12 @@ package com.danielsoo.blog_daniel.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
 import com.danielsoo.blog_daniel.service.posts.PostsService;
+import com.danielsoo.blog_daniel.web.dto.PostsResponseDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +24,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
