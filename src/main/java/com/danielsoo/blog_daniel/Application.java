479b239616775577136ce701c445902280b24852
Dongsoo Kim <william@uzen.io>
2022-06-06 09:29:05 +0900

    Add Annotation Study
    Add Annotation Study

--- src/main/java/com/danielsoo/blog_daniel/Application.java
+++ src/main/java/com/danielsoo/blog_daniel/Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication // 항상 최상단에서 사용, 여기서부터 읽기 시작함
 public class Application {
     public static void main(String[] args){
+        //내장 WAS를 어플리케이션 실행할 때 같이 실행. 항성 서버에 톰캣을 설치할 필요없이, Jar파일로 실행하면 끝
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/com/danielsoo/study/javaFundamental/AnnotationStudy.java
@@ -0,0 +1,18 @@
+package com.danielsoo.study.javaFundamental;
+
+public class AnnotationStudy {
+    /* Spring boot
+        1. Start
+            1) @SpringBootApplication: 항상 최상단에서 사용, 여기서부터 읽기 시작함
+            2)
+        2. Controller
+            1) 상단
+                - @RestController: 컨트롤러를 JSON으로 반환하는 컨트롤러로 만들어 준다. @ResponseBody를 각 메소드마다 선언했던 것을 한번에 사용할 수 있게 함.
+            2) 메소드별
+                - @RequestMapping: (method=RequestMethod.GET) 등을 붙여서 HTTP METHOD를 받게한 옛날 방식
+                - @GetMapping: HTTP Method의 Get 요청을 받을 수 있는 API를 만듦,
+        3.
+
+
+    */
+}
