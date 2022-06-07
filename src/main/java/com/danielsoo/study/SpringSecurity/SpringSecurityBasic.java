dd3f3838d7d8f229af2f222d717e26218ee527e6
Dongsoo Kim <william@uzen.io>
2022-06-07 14:11:15 +0900

    Spring Security Basic
    Spring Security Basic

---
+++ src/main/java/com/danielsoo/study/SpringSecurity/SpringSecurityBasic.java
@@ -0,0 +1,21 @@
+package com.danielsoo.study.SpringSecurity;
+
+/**
+ @5BasicConcepts
+ 1. Authentication
+    - Define who you are
+    1) Knowledge based authentication: pw, pinCode, SecretQuestions
+    2) Possession based authentication: TxtMsg, KeyCards, Badges, TokenDevice
+    3) MultiFactor Authentication: Knowledge + Possession
+ 2. Authorization
+    - Are you allowed to do this? YN
+ 3. Principal
+    - Currently loggedIn user
+    - 1 request can have only 1 principal
+ 4. Granted Authority
+ 5. Roles
+    - Group of Authorities
+ */
+
+public class SpringSecurityBasic {
+}
---
+++ src/main/java/com/danielsoo/study/javaFundamental/CollectionFrameworkStudy.java
@@ -0,0 +1,26 @@
+package com.danielsoo.study.javaFundamental;
+
+import java.util.Collection;
+
+/**
+@Interfaces
+ 1. List: ArrayList, LinkedList, Stack, Vector - 순서O중복O
+ 2. Set: HashSet, TreeSet  - 순서x중복x
+ 3. Map: HashMap, TreeMap, Hashtable, Properties - 순서x중복x
+ */
+
+public class CollectionFrameworkStudy {
+ /**
+  @CollectionInterface
+  1. C: boolean add(E e), boolean addAll(Collection c),
+  2. R: boolean contains(Object o), boolean containsAll(Collection c), boolean equals(Object o), boolean isEmpty(), int size()
+  3. U:
+  4. D: void clear(), boolean remove(E e), boolean removeAll(Collection c)
+  5. O: Iterator iterator(), Object[] toArray(), Object[] toArray(Object[] a)
+  */
+
+
+ public static void main(String... args){
+
+ }
+}
